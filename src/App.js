cc838179460083d57c552782f2db4fb9b621649e
al-shaimon <alshaimon968@gmail.com>
2022-10-10 01:13:14 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+function App() {
+  const router = createBrowserRouter([
+    {path: '/', element: <div>This is Default page</div>},
+    {path: '/home', element: <div>Home Page</div>},
+    {path: '/about', element: <div>This is About Page</div>},
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
