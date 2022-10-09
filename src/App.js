ee1f62e498aceec9dbe1d2946069bc290cb872b8
al-shaimon <alshaimon968@gmail.com>
2022-10-10 02:02:06 +0600

    nested route, outlet

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Products from './components/Products/Products';
+import Main from './Layout/Main';
+import Friends from './components/Friends/Friends';
 
 function App() {
   const router = createBrowserRouter([
-    {path: '/', element: <div>This is Default page</div>},
-    {path: 'home', element: <Home></Home>},
-    {path: 'about', element: <About></About>},
-    {path: 'products', element: <Products></Products>}
-  ])
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        { path: 'home', element: <Home></Home> },
+        { path: '/', element: <Home></Home> },
+        { path: 'products', element: <Products></Products> },
+        {path: 'friends', element: <Friends></Friends>}
+      ],
+    },
+    { path: 'about', element: <About></About> },
+  ]);
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
