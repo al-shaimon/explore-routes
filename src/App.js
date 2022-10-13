88730cb71139eeab18eded041f2854876aefa0d7
al-shaimon <alshaimon968@gmail.com>
2022-10-13 18:19:48 +0600

    dynamic route recap

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Main from './Layout/Main';
 import Friends from './components/Friends/Friends';
 import FriendDetails from './components/FriendDetails/FriendDetails';
 import Posts from './components/Posts/Posts';
+import PostDetails from './components/PostDetails/PostDetails';
 
 function App() {
   const router = createBrowserRouter([
@@ -18,27 +19,34 @@ function App() {
         { path: 'home', element: <Home></Home> },
         { path: '/', element: <Home></Home> },
         { path: 'products', element: <Products></Products> },
-        { 
-          path: 'friends', 
-          loader: async() =>{
-            return fetch('https://jsonplaceholder.typicode.com/users')
+        {
+          path: 'friends',
+          loader: async () => {
+            return fetch('https://jsonplaceholder.typicode.com/users');
           },
-          element: <Friends></Friends> 
+          element: <Friends></Friends>,
         },
         {
-          path:'/friend/:friendId',
-          loader: async ({params}) => {
+          path: '/friend/:friendId',
+          loader: async ({ params }) => {
             // console.log(params.friendId)
-            return fetch(`https://jsonplaceholder.typicode.com/users/${params.friendId}`)
+            return fetch(`https://jsonplaceholder.typicode.com/users/${params.friendId}`);
           },
-          element: <FriendDetails></FriendDetails>
+          element: <FriendDetails></FriendDetails>,
         },
         {
           path: 'posts',
-          loader: async() => {
-            return fetch('https://jsonplaceholder.typicode.com/posts')
+          loader: async () => {
+            return fetch('https://jsonplaceholder.typicode.com/posts');
           },
-          element: <Posts></Posts>
+          element: <Posts></Posts>,
+        },
+        {
+          path: '/post/:postId',
+          loader: async({params}) =>{
+            return fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+          },
+          element: <PostDetails></PostDetails>
         }
       ],
     },
